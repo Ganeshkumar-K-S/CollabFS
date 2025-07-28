073d9084b520fee980021df54efc1c9abd8f19d7
Ganeshkumar-k-s <ganeshkumar78602005@gmail.com>
2025-07-28 22:03:21 +0530

    file api authentication

--- server/app/routes/file_services.py
+++ server/app/routes/file_services.py
@@ -1,10 +1,25 @@
 from app.db.connection import fs
-from fastapi import APIRouter, UploadFile, File, HTTPException,Request
+from fastapi import APIRouter, UploadFile, File, HTTPException,Request,Depends
+from starlette.status import HTTP_403_FORBIDDEN
 from bson import ObjectId
+from dotenv import load_dotenv
+load_dotenv()
+import os
+
 
 file_engine = APIRouter(prefix="/file")
 
-@file_engine.post("/upload") 
+def verify_file_api(request : Request):
+    expected_key=os.getenv('FILE_API_KEY')
+    key_name="x-api-key"
+    response_key=request.headers.get(key_name)
+    if expected_key!=response_key:
+        raise HTTPException(
+            status_code=HTTP_403_FORBIDDEN,
+            detail="Unauthorized access"
+        )
+
+@file_engine.post("/upload",dependencies=[Depends(verify_file_api)]) 
 async def upload_file(file: UploadFile = File(...)):
     try:
         contents = await file.read()
@@ -14,7 +29,7 @@ async def upload_file(file: UploadFile = File(...)):
         raise HTTPException(status_code=500, detail=str(e))
 
 
-@file_engine.delete("/delete")
+@file_engine.delete("/delete",dependencies=[Depends(verify_file_api)])
 async def delete_file(request: Request):
     try:
         data=await request.json()
